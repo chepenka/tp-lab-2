551c76a1fef4aad00d6be6c75ee63fba0655c63d
Anghellia <son0shad@gmail.com>
2019-11-03 12:49:32 +0300

    assignment

---
+++ src/main3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "task3.h"
+
+template <typename T> T change(T a)
+{
+	return a * a;
+}
+
+int main() {
+	const int n = 5;
+	int nums[n] = { 1, 2, 3, 4, 5 };
+	double doubles[n] = { 1.2, 2.3, 3.4, 4.5, 5.6 }; 
+	
+	map<int, n>(nums, change);	
+	map<double, n>(doubles, change);
+
+	for (int i = 0; i < n; i++)
+		std::cout << nums[i] << " ";
+	std::cout << std::endl;
+	
+	for (int i = 0; i < n; i++)
+		std::cout << doubles[i] << " ";
+	std::cout << std::endl;
+}
