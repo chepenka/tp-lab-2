bfea5c5b52cc9a5f48a740d783e1ec3af60cb6f6
Artyom Pyanzin <aapyanzin@edu.hse.ru>
2019-11-05 22:10:07 +0300

    zzz

--- src/main2.cpp
+++ src/main2.cpp
@@ -2,31 +2,31 @@
 #include "string.h"
 using namespace std;
 
-int main()
+template<typename T>
+T gen()
 {
 
-	template<typename T>
-	T gen()
-	{
+	static int t = 48;
+	return t++;
+}
 
-		static int t = 48;
-		return t++;
-	}
+template<>
+char* gen()
+{
 
-	template<>
-	char* gen()
-	{
+	static unsigned t = 0;
+	t++;
+	char* s = new char[t + 1];
 
-		static unsigned t = 0;
-		t++;
-		char* s = new char[t + 1];
+	for (size_t i = 0; i < t; i++)
+		s[i] = '0' + i;
+	s[t] = '\0';
 
-		for (size_t i = 0; i < t; i++)
-			s[i] = '0' + i;
-		s[t] = '\0';
+	return s;
+}
 
-		return s;
-	} 
+int main()
+{
 
 	const size_t N = 5;
 	char **newArr;
--- src/main3.cpp
+++ src/main3.cpp
@@ -2,22 +2,25 @@
 #include "string.h"
 using namespace std;
 
+
+template<class T>
+T change(T val)
+{
+	return val + 1;
+}
+
+template<>
+char* change(char* val)
+{
+	size_t n = strlen(val);
+	for (size_t i = 0; i < n; i++)
+		val[i] = val[i] + 1;
+	return val;
+}
+
 int main()
 {
-	template<class T>
-	T change(T val)
-	{
-		return val + 1;
-	}
-
-	template<>
-	char* change(char* val)
-	{
-		size_t n = strlen(val);
-		for (size_t i = 0; i < n; i++)
-			val[i] = val[i] + 1;
-		return val;
-	}
+	
 	const size_t n = 5;
 	int arr[n] = { 1, 2, 3, 4, 5 };
 	map<int, n>(arr, change);
