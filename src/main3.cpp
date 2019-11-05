303218b344cc8cec7079ddddca12a84f892393d3
avmusatov <antidote17@yandex.ru>
2019-11-05 23:10:55 +0300

    18PMI-2 Anton Musatov

--- src/main3.cpp
+++ src/main3.cpp
@@ -8,14 +8,6 @@ T change(T val)
 	return val + 1;
 }
 
-template<>
-char* change(char* val)
-{
-	size_t n = strlen(val);
-	for (size_t i = 0; i < n; i++)
-		val[i] = val[i] + 1;
-	return val;
-}
 
 int main() {
 	char mas[5] = { '1','2','3','4','5' };
