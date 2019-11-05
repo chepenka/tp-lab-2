623aa507e6bf759afa772a00402590682007b1a2
Doroshenko Ilya <45979573+iddoroshenko@users.noreply.github.com>
2019-11-05 19:06:09 +0300

    const size_t& N

--- include/task3.h
+++ include/task3.h
@@ -2,7 +2,7 @@
 
 #include "string.h"
 
-template <class T, const int N>
+template <class T, const size_t& N>
 void map(T* mas, T(*change)(T)){
     for(int i = 0; i < N; i++)
         mas[i] = change(mas[i]);
