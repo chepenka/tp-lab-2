0761276cb515e13b5f6a7bbb6ae183c89a37502c
Максим Захаров <49262152+FLTLN@users.noreply.github.com>
2019-11-10 14:23:52 +0300

    Change dependencies to avoid redefenitions

--- include/task2.h
+++ include/task2.h
@@ -7,15 +7,6 @@ template<class T> T own_gen()
 	return t++;
 }
 
-template <class T> void printArr(T* arr, size_t len)
-{
-	for (size_t i = 0; i < len; i++)
-	{
-		std::cout << arr[i] << " ";
-	}
-	std::cout << std::endl;
-}
-
 template<typename T, const size_t size> T* createArr(T(*gen)()) {
 	T* arr = new T[size];
 	for (size_t i = 0; i < size; i++)
