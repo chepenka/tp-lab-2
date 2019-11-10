75a4b11bb15b469bc2af07557a33262b34bc48e7
Максим Захаров <49262152+FLTLN@users.noreply.github.com>
2019-11-10 15:31:54 +0300

    wip

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,5 +1,25 @@
 #include "task2.h"
-#include "task1.h"
+
+#include <iostream>
+#include <string>
+
+template <class T> void printArr(T* arr, size_t len)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+template <> void printArr(char** arr, size_t len)
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		std::string out(arr[i]);
+		std::cout << out << " ";
+	}
+	std::cout << std::endl;
+}
 
 int main()
 {
