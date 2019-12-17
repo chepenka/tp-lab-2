0b063b792cab2b78172ee7bebcc1ece7fe0deecf
chepenka <51460032+chepenka@users.noreply.github.com>
2019-12-17 23:50:43 +0300

    Update main3.cpp

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
+#include <cstring>
 #include "task3.h"
 
-using namespace std;
 
 template<class T>
-T change(T elem) {
-	return elem * elem;
+T change(T value) {
+    return value + 228;
+}
+
+template<>char* change(char* value) {
+    for (size_t i = 0; i < strlen(value); i++) {
+        value[i] *= 10;
+    }
+    return value;
 }
 
 int main() {
-	const int N = 5;
-	int* Arr = new int[n];
-	for (int i = 1; i < 6; i++) {
-		Arr[i - 1] = i;
-	}
-	map<int, N>(Arr, change);
-	for (int i = 0; i < N; i++) {
-		cout << Arr[i] << "\n";
-	}
-	return 0;
+    const size_t size = 6;
+    int array[size]{ 31, 13, 13, 7, 5, 6};
+    map<int, size>(array, change);
+    for (int i = 0; i < size; i++) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << "\n";
+    return 0;
 }
