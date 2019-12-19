f8742f71d98a45c5a4fc4c60ebc6ca5a4e0b3c27
chepenka <51460032+chepenka@users.noreply.github.com>
2019-12-19 16:34:07 +0300

    Update main1.cpp

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,42 +1,6 @@
-/* #include <iostream>
-#include "task1.h"
-
-using namespace std;
-
-int main() {
-	int n;
-	cin >> n;
-	char* str[5] = { (char*)"Nizkaya", (char*)"zhizn`", (char*)"ya", (char*)"svoih", (char*)"topchy" };
-	msort(str, n);
-	for (int i = 0; i < n; i++) {
-		cout << str[i] << "\n";
-	}
-
-	int* mas = new int[n];
-	for (int i = 0; i < n; i++) {
-		mas[i] = n - i;
-	}
-	msort(mas, n);
-	for (int i = 0; i < n; i++) {
-		cout << mas[i] << "\n";
-	}
-	cin >> n;
-	delete[] mas;
-	return 0;
-} */
-#include <iostream>
 #include "task1.h"
 
 int main() {
-    int array[] = {228,229, 54, 2, 55, 532, 2876};
-    for (int i = 0; i < 7; i++) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << "\n";
-    msort(array,7);
-    for (int i = 0; i < 7; i++) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << "\n";
-    return 0;
+	char* val[6]{ (char*)"uhh", (char*)"kak", (char*)"byi", (char*)"ne", (char*)"vyletet", (char*)"mne" };
+	msort(val, 6);
 }
